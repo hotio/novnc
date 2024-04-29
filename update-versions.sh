a95054a6ff3291b8f64e19ea004bf1034d131f26
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-04-29 11:11:13 +0200

    build noVNC

--- update-versions.sh
+++ update-versions.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-rtorrent_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/jesec/rtorrent/releases/latest" | jq -re .tag_name) || exit 1
-[[ -z ${rtorrent_version} ]] && exit 0
-[[ ${rtorrent_version} == null ]] && exit 0
-flood_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/jesec/flood/releases/latest" | jq -re .tag_name) || exit 1
-[[ -z ${flood_version} ]] && exit 0
-[[ ${flood_version} == null ]] && exit 0
-version="${rtorrent_version}--${flood_version}"
+version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/novnc/novnc/releases/latest" | jq -re .tag_name) || exit 1
+[[ -z ${version} ]] && exit 0
+[[ ${version} == null ]] && exit 0
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
-    --arg flood_version "${flood_version//v/}" \
-    --arg rtorrent_version "${rtorrent_version//v/}" \
-    '.version = $version | .flood_version = $flood_version | .rtorrent_version = $rtorrent_version' <<< "${json}" | tee VERSION.json
+    '.version = $version' <<< "${json}" | tee VERSION.json
